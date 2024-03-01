1c5dea01d345b4234d4bd08bfb8030451db9ed66
WilliamSepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-03-01 09:33:52 -0500

    first commit

---
+++ storage/detalle_pedido.py
@@ -0,0 +1,9 @@
+detalle_pedido = [
+    {
+        "codigo_pedido": 0,
+        "codigo_producto": "",
+        "": "",
+        "": "",
+        "": "",
+    }
+]
---
+++ storage/pedido.py
@@ -0,0 +1,11 @@
+pedido = [
+    {
+        "codiogo_pedido": 0,
+        "fecha_pedidio": "2024/03/01", 
+        "fecha_esperada": "2024/03/01",
+        "fecha_entrega": "2024/03/01",
+        "estado": "",
+        "comentario": "",
+        "codigo_clente": 0
+    }
+]
