bab2b27e16a710bae87515a2999ca23931f72df6
WilliamSepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-03-06 13:55:10 -0500

    punto # 5 filtros

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ import modules.getClients as clientes
 import modules.getOficina as Oficina 
 import modules.getEmpleados as empleados
 
-# print(cliente.getAllClientcodigo_empleado_rep_ventas("codigo_empleado_rep_ventas"))
-
-# print(tabulate(empleados.getAllNombreApellidoemail(7), tablefmt= 'gird'))
-
-print(tabulate(Oficina.getAllCodigoCiudad(), tablefmt= 'gird'))
+print(tabulate(empleados.getAllNombreApellidoPuestos(), tablefmt= 'gird'))
 
 
 
--- modules/getEmpleados.py
+++ modules/getEmpleados.py
@@ -1,39 +1,52 @@
 # devuelve un listado con el nombre, apellidos, y email 
 # de los empleados cuyo  jefe tiene un codigo de jefe igual  a 7 
 
+# import storage.empleado as em
+
+# def getAllNombreApellidoemail(codigo):
+#     NombreApellidoemail = []
+#     for val in em.empleados:
+#         if(val.get("codigo_jefe") == codigo):
+#             NombreApellidoemail.append(
+#                 {
+#                     "nombre": val.get("nombre"),
+#                     "Apellidos": f'{val.get("apellido1")} {val.get("apellido2")}',
+#                     "email": val.get("email"),
+#                     "jefe": val.get("codigo_jefe")
+#                 }
+#             )
+#     return NombreApellidoemail
+
+# # devuelve el nombre, puesto, apellido y email del jefe de la empresa 
+# import storage.empleado as em 
+
+# def getAllNombrePuestoApellidoEmailJefe():
+#     NombrePuestoApellidoEmailJefe = []
+#     for val in em.empleados:
+#         if(val.get("codigo_jefe")) == None:
+#             NombrePuestoApellidoEmailJefe.append(      
+#                 {                  
+#                 "puesto": val.get("puesto"),
+#                 "nombre": val.get("nombre"),
+#                 "apellidos": f'{val.get("apellido1")} {val.get("apellido2")})',
+#                 "email": val.get("email"),
+#                 "jefe": val.get("jefe") 
+#                 }
+#             )
+#         return NombrePuestoApellidoEmailJefe
+    
+#Devuelve un listado con el nombre, apellidos y puesto de aquellos empleados que no sean representantes de ventas.
 import storage.empleado as em
-
-def getAllNombreApellidoemail(codigo):
-    NombreApellidoemail = []
+def getAllNombreApellidoPuestos():
+    NombreApellidoPuestos = []
     for val in em.empleados:
-        if(val.get("codigo_jefe") == codigo):
-            NombreApellidoemail.append(
-                {
-                    "nombre": val.get("nombre"),
-                    "Apellidos": f'{val.get("apellido1")} {val.get("apellido2")}',
-                    "email": val.get("email"),
-                    "jefe": val.get("codigo_jefe")
-                }
-            )
-    return NombreApellidoemail
-
-# devuelve el nombre, puesto, apellido y email del jefe de la empresa 
-import storage.empleado as em 
-
-def getAllNombrePuestoApellidoEmailJefe():
-    NombrePuestoApellidoEmailJefe = []
-    for val in em.empleados:
-        if(val.get("codigo_jefe")) == None:
-            NombrePuestoApellidoEmailJefe.append(      
-                {                  
-                "puesto": val.get("puesto"),
-                "nombre": val.get("nombre"),
-                "apellidos": f'{val.get("apellido1")} {val.get("apellido2")})',
-                "email": val.get("email"),
-                "jefe": val.get("jefe") 
-                }
-            )
-        return NombrePuestoApellidoEmailJefe
+        if (val.get("puesto") != 'Representante Ventas'):
+            NombreApellidoPuestos.append({
+            "nombre": val.get("nombre"),
+            "Apellidos": f'{val.get("apellido1")} {val.get("apellido2")}',
+            "puesto": val.get("puesto")
+        })
+    return NombreApellidoPuestos 
         
      
 
