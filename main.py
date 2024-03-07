ff7ef61308b1670efd838762d4ba1a60125fd0fc
WilliamSepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-03-07 13:48:03 -0500

    solucion punto_numero 10

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import modules.getClients as clientes
 import modules.getOficina as Oficina 
 import modules.getEmpleados as empleados
 import modules.getpedido as pedido
-print(tabulate(pedido.getAllPedidosEntregadosAtrasadosDeTiempo(), tablefmt='rounded_grid'))
+
+print(tabulate(pedido.getAllPedidosEntregadosDosDiasAntes(), tablefmt='rounded_grid'))
 
 
 
--- modules/getpedido.py
+++ modules/getpedido.py
@@ -27,27 +27,33 @@ def getAllPedidosEntregadosAtrasadosDeTiempo():
                     "fecha_de_entrega": val.get("fecha_entrega"),
                 })
     return pedidosEntregado
+# solucion punto numero 10
+# devuelve un listado con el codigo del pedido, codigo cliente, fecha esperada y fecha de entrega de los pedidios cuya 
+#fecha de entrega ha sido al menos dos dias antes de la fecha esperada 
 
+import storage.pedido as pe
+from datetime import datetime
 
+import storage.pedido as pe
+from datetime import datetime
 
+def getAllPedidosEntregadosDosDiasAntes():
+    pedidosEntregado = []
+    for pedido in pe.pedido:
+        if pedido.get("estado") == "Entregado" and pedido.get("fecha_entrega") is None:
+            pedido["fecha_entrega"] = pedido.get("fecha_esperada")
+        if pedido.get("estado") == "Entregado":
+            date_1 = "/".join(pedido.get("fecha_entrega").split("-")[::-1])
+            date_2 = "/".join(pedido.get("fecha_esperada").split("-")[::-1])
 
-# def getAllPedidosEntregadosAtrasadosDeTiempo():
-#     pedidosEntregado = []
-#     for pedidos in pe.pedido:
-#         if (pedidos.get("estado") == "Entregado" and pedidos.get("fecha_entrega") is None):
-#             pedidos["fecha_entrega"]= pedidos.get("fecha_esperada")
-#         if pedidos.get("estado") == "Entregado":
-
-#             date_1 = "/".join(pedidos.get("fecha_entrega").split("-")[::-1])
-#             date_2 = "/".join(pedidos.get("fecha_esperada").split("-")[::-1])
-#             start = datetime.strptime(date_1, "%d/%m/%Y")
-#             end = datetime.strptime(date_2, "%d/%m/%Y")
-#             diff = end.date() - start.date()
-#             if diff.days < 0 :
-#                 pedidosEntregado.append({
-#                     "codigo_pedido": pedidos.get("codigo_pedido"),
-#                     "codigo_cliente": pedidos.get("codigo_cliente"),
-#                     "fecha_esperada": pedidos.get("fecha_esperada"),
-#                     "fecha_de_entrega": pedidos.get("fecha_entrega")
-#                 })
-#     return pedidosEntregado
+        start = datetime.strptime(date_1, "%d/%m/%Y")
+        end = datetime.strptime(date_2, "%d/%m/%Y")
+        diff = end.date() - start.date()
+        if diff.days >= 2:
+                pedidosEntregado.append({
+                    "código_de_pedido": pedido.get("codigo_pedido"),
+                    "código_de_cliente": pedido.get("codigo_cliente"),
+                    "fecha_esperada": pedido.get("fecha_esperada"),
+                    "fecha_de_entrega": pedido.get("fecha_entrega"),
+                })
+    return pedidosEntregado
